a225c7c3155c7dfd5c1846e91553112fe39bdcb0
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-09-11 09:37:26 +0200

    add img, name and types

--- src/App.js
+++ src/App.js
@@ -3,20 +3,38 @@ import './App.css';
 import pokemons from './pokemons';
 
 class App extends React.Component {
-  /* constructor(props) {
+  constructor(props) {
     super(props);
+
+    this.state = {
+      pokemons: pokemons
+    };
   }
- */
+
   render() {
     return (
       <div className="App">
         <h1 className="App__title">Mi lista de pokemon</h1>
         <ul className="pokemonList">
-          {pokemons.map(pokemon => {
+          {this.state.pokemons.map(pokemon => {
             return (
-              <li className="card">
+              <li className="card" key={pokemon.id}>
                 <div className="cardContainer">
-                  
+                  <div className="imgContainer"><img src={pokemon.url} alt={pokemon.name} className="pokemonImg" /></div>
+                  <div className="pokemonName">
+                    <h2 className="pokemonTitle">{pokemon.name}</h2>
+                  </div>
+                  <ul className="pokemonsType">
+
+                    {pokemon.types.map(type => {
+                      return (
+                        <li className="pokemonType" key={pokemon.id}>
+                          {type}
+                        </li>
+                      );
+                    })}
+
+                  </ul>
                 </div>
               </li>
             );
